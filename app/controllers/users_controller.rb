0a9ab6bc6fd61904d37fa64f103401bce904b3ae
BenWix <benwichser@gmail.com>
2020-05-07 18:26:26 -0400

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,61 @@
 class UsersController < ApplicationController
+    get '/signup' do 
+        if logged_in?
+            redirect '/tweets'
+        end
+        erb :"users/create_user"
+    end
 
+    post '/signup' do
+        unless params[:username] == "" || params[:email] == "" 
+            user = User.new(username: params[:username], email: params[:email], password: params[:password])
+            if user.save
+                session[:user_id] = user.id
+                redirect '/tweets'
+            end
+        end
+        redirect '/signup'
+    end
+
+    get '/login' do 
+        unless logged_in?
+            erb :'users/login'
+        else 
+            redirect '/tweets'
+        end
+    end
+
+    post '/login' do 
+        user = User.find_by(username: params[:username])
+        #binding.pry
+
+        if user && user.authenticate(params[:password])
+            session[:user_id] = user.id
+            redirect '/tweets'
+        else 
+            redirect '/login'
+        end
+    end
+
+    get '/logout' do 
+        if logged_in?
+            session.clear 
+        end
+        redirect '/login'
+    end
+
+    post '/logout' do 
+        session.clear
+        redirect '/login'
+    end
+
+    get '/users/:slug' do
+        # if logged_in?
+            @user = User.find_by_slug(params[:slug])
+            erb :'users/show'
+        # else 
+        #     redirect '/login'
+        # end 
+    end 
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,15 @@
 class User < ActiveRecord::Base
   has_secure_password
   has_many :tweets
+
+  def slug 
+    #binding.pry
+    username.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')
+  end
+
+  def self.find_by_slug(phrase)
+    User.all.each do |user|
+      return user if user.slug == phrase
+    end
+  end
 end
