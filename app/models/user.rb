ef611bd768a2fd022b8b559c1a991334c1a8501c
Christopher <reinkendigital@gmail.com>
2020-05-12 12:42:58 -0500

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,18 @@
 class User < ActiveRecord::Base
-  has_secure_password
-  has_many :tweets
+    has_secure_password
+    has_many :tweets
+
+    def slug
+        "#{self.username.gsub(/\s/,'-')}"
+    end
+
+    def self.find_by_slug(comparison)
+        @user = nil
+        self.all.each do |user|
+            if user.slug == comparison
+                @user = user
+            end
+        end
+        @user
+    end
 end
